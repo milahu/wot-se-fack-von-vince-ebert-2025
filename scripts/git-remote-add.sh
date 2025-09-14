967ef5b07cc491a316547d3d9fbbf67895da040f
Milan Hauth <milahu@gmail.com>
2025-09-14 14:03:04 +0200

    up scripts

--- scripts/git-remote-add.sh
+++ scripts/git-remote-add.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+set -eux
+
+owner=$(cat ~/.git-credentials | grep '@github\.com$' | sed -E 's|https://([^:]+):.*$|\1|')
+if [ -z "$owner" ]; then
+  echo "error: failed to parse repo owner from ~/.git-credentials"
+  exit 1
+fi
+
+cd "$(dirname "$0")/.."
+repo=$(basename "$PWD")
+
 function git_remote_add_onion() {
   local remote="$1"
   local url="$2"
@@ -8,6 +19,14 @@ function git_remote_add_onion() {
   git config --add remote."$remote".proxy socks5h://127.0.0.1:9050
 }
 
-git_remote_add_onion darktea.onion http://it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/milahu/darknet-git-hosting-services
+remote=darktea.onion
+url=http://it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/$owner/$repo
+git_remote_add_onion "$remote" "$url"
 
-git_remote_add_onion righttoprivacy.onion http://gg6zxtreajiijztyy5g6bt5o6l3qu32nrg7eulyemlhxwwl6enk6ghad.onion/milahu/darknet-git-hosting-services
+remote=righttoprivacy.onion
+url=http://gg6zxtreajiijztyy5g6bt5o6l3qu32nrg7eulyemlhxwwl6enk6ghad.onion/$owner/$repo
+git_remote_add_onion "$remote" "$url"
+
+remote=darkforest.onion
+url=http://git.dkforestseeaaq2dqz2uflmlsybvnq2irzn4ygyvu53oazyorednviid.onion/$owner/$repo
+git_remote_add_onion "$remote" "$url"
